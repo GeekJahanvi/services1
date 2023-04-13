c6358fd64b56734b315b54b19b52cb25015c005a
Jahanvi <jahanvi@geekyants.com>
2023-04-13 11:53:55 +0530

    frgrf

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './server.ts',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.ts',
+    path: path.resolve(__dirname, 'src'),
   },
 };
